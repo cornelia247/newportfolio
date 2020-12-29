1a812c384fc6df4dddb5d0502571d3ffe3dfae04
Ekpenyong Cornelia <ekpenyongcornelia2@gmail.com>
2020-12-29 01:42:55 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.nav-bar').toggleClass('open');
+    });
+
+});
+const navSlide = () => {
+    const burger = document.querySelector('.menu-toggler');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-open');
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s'
+            }
+        });
+    
+    });
+}
+navSlide();
+
